1354ca57725ea590e1f5ebcbaf278bb31dbfa528
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-02-07 14:24:40 +0100

    added materials/ door unlock mechanism

--- Assets/Scripts/WheelBehavior.cs
+++ Assets/Scripts/WheelBehavior.cs
@@ -5,17 +5,51 @@ using TMPro;
 
 public class WheelBehavior : MonoBehaviour
 {
+    int intRotation, prevRotation;
+    bool chevron1, chevron2, chevron3, chevron4;
     [SerializeField] TMP_Text rotationCounter;
+    [SerializeField] Material greenMat;
+    [SerializeField] Renderer voyant1, voyant2, voyant3, voyant4;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rotationCounter.text = gameObject.transform.localEulerAngles.x.ToString();
+        //code 310 - 25 - 300 - 10
+        intRotation = Mathf.FloorToInt(gameObject.transform.localEulerAngles.y);
+        rotationCounter.text = intRotation.ToString();
+        if (!chevron4)
+        {
+            if (intRotation == 310 && prevRotation == 311 && !chevron1)
+            {
+                chevron1 = true;
+                voyant1.material = greenMat;
+            }
+            if (intRotation == 25 && prevRotation == 24 && chevron1)
+            {
+                chevron2 = true;
+                voyant2.material = greenMat;
+
+            }
+            if (intRotation == 300 && prevRotation == 301 && chevron2)
+            {
+                chevron3 = true;
+                voyant3.material = greenMat;
+
+            }
+            if (intRotation == 10 && prevRotation == 9 && chevron3)
+            {
+                chevron4 = true;
+                voyant4.material = greenMat;
+                //call door open here
+                
+            }
+        }
+        prevRotation = intRotation;
     }
 
 }
